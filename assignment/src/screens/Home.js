f3579e2b0af43089bf20451d03c973c43a2d5b71
ramanujan123 <mitulvardhan@iitbhilai.ac.in>
2024-01-30 02:11:09 +0530

    added the search bar functionality

--- assignment/src/screens/Home.js
+++ assignment/src/screens/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
-import Card_shop from '../components/card_shop'
+import { Link } from 'react-router-dom';
+import Card_shop from '../components/card_shop';
 import Carousel from '../components/carousel';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import './Home.css'
+import './Home.css';
 
 export default function Home() {
   const [search, setSearch] = useState('');
@@ -12,16 +12,16 @@ export default function Home() {
 
   const loadData = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/shopData", {
+      const response = await fetch('http://localhost:5000/api/shopData', {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       });
       const data = await response.json();
       setShops(data[0]);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
   };
 
@@ -29,11 +29,16 @@ export default function Home() {
     loadData();
   }, []);
 
+  const handleSearchChange = (searchValue) => {
+    // Update the search state when the Carousel search changes
+    setSearch(searchValue);
+  };
+
   return (
-    <div className='coloring'>
+    <div className="coloring">
       <Navbar />
-      <Carousel />
-      <div className='container' style={{ marginTop: '30px' }}>
+      <Carousel onSearchChange={handleSearchChange} />
+      <div className="container" style={{ marginTop: '30px' }}>
         <div className="row">
           <div className="col-12">
             <h2 className="font-weight-bold mt-6 mb-6">Restaurants</h2>
@@ -43,9 +48,9 @@ export default function Home() {
         {shops.length !== 0 ? (
           <div className="row">
             {shops
-              .filter(item => item.name.toLowerCase().includes(search.toLocaleLowerCase()))
-              .map(filterItem => (
-                <div key={filterItem._id} className='col-12 col-md-6 col-lg-4 mt-3'>
+              .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+              .map((filterItem) => (
+                <div key={filterItem._id} className="col-12 col-md-6 col-lg-4 mt-3">
                   <Link to={`/shop/${filterItem._id}`} className="link">
                     <Card_shop
                       shopName={filterItem.name}
@@ -55,8 +60,7 @@ export default function Home() {
                     />
                   </Link>
                 </div>
-              ))
-            }
+              ))}
           </div>
         ) : (
           <div>"No such data found"</div>
