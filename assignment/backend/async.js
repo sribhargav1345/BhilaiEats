92898138a535645be818257de10d47089453dfc8
PrathmeshJirafe <prathmeshn@iitbhilai.ac.in>
2024-02-11 17:03:35 +0530

    nothing new

---
+++ assignment/backend/async.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+const fetchData = async (id) => {
+    const results = await axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    // console.log(results);
+    return results.data;
+}
+fetchData(1);
+
+module.exports = fetchData;
