7f4136949e05182b3820ed57165d9f4da979097d
PrathmeshJirafe <prathmeshn@iitbhilai.ac.in>
2024-02-13 23:08:01 +0530

    no major changes

---
+++ assignment/backend/index.test.js
@@ -0,0 +1,13 @@
+const app = require("./index.js")
+const request = require("supertest")
+
+describe("GET /users", () => {
+    describe("given a username and password", () => {
+
+        test("should respond with a 200 status code", async () => {
+        //     const response = await request(app).get("/user")
+        //     expect(response.statusCode).toBe(200)
+        expect(true).toBe(true);
+        })
+    })
+})
