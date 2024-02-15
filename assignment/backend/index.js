ac6c4a0c7ce41956b9211361432cc07041f9ad16
sribhargav1345 <sribhargav1345@gmail.com>
2024-02-15 22:57:59 +0530

    Frontend deploy

--- assignment/backend/index.js
+++ assignment/backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://bhilai-eats-nm.vercel.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
